17618e01a8327da70828d6cd77e54ba5768cfeb9
Sayan Karmakar <ironheartz367@gmail.com>
2024-10-27 13:46:44 +0530

    Update settings.py

--- backend/openblogs/openblogs/settings.py
+++ backend/openblogs/openblogs/settings.py
@@ -157,6 +157,6 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "djangoatservice@gmail.com"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST', 'your_default_email')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'your_default_email_password')
 EMAIL_USE_TLS = True
